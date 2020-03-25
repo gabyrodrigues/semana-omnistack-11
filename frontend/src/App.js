35060970f74926b90d1b75a0cff778b90cfba74f
Gabriely Rodrigues <gabyrlima.99@gmail.com>
2020-03-25 16:49:15 -0300

    limpeza do projeto frontend + conceitos react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Header from './Header';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Hello Codes
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [counter, setCounter] = useState(0); //sempre que for preciso que o componente armazene uma informação nele, é preciso utilizar um estado
+
+    function increment() {
+        setCounter(counter + 1);
+    }
+
+    return (
+        <div>
+            <Header> Contador: {counter} </Header>
+            <button onClick={increment}>Incrementar</button>
+        </div>
+    );
 }
 
 export default App;
