82a2472e9258fac3649fcdd1a156a3fd24cd12c8
Gabriely Rodrigues <gabriely.bleez@gmail.com>
2020-05-26 22:35:24 -0300

    vericando se id de ong a ser criada já existe

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -1,6 +1,23 @@
 const generateUniqueId = require('../utils/generateUniqueId');
 const connection = require('../database/connection');
 
+function generateId() {
+    return checkId(generateUniqueId());
+}
+
+async function checkId(id) { //verifica se id criado randomicamente já existe
+    const checkOngId = await connection('ongs')
+        .select('*')
+        .where('id', id)
+        .first();
+    
+    while (checkOngId) { //enquanto o id criado existir ele vai criando um novo
+        id = generateId();
+    }
+
+    return id;
+}
+
 module.exports = {
     async index (request, response) {
         const ongs = await connection('ongs').select('*');
@@ -11,7 +28,7 @@ module.exports = {
     async create(request, response) {
         const { name, email, whatsapp, city, uf } = request.body;
 
-        const id = generateUniqueId();
+        const id = await generateId();
 
         await connection('ongs').insert({ //como é assincrono, ele espera essa função terminar para seguir com o resto do código
             id,
@@ -20,7 +37,7 @@ module.exports = {
             whatsapp,
             city,
             uf
-        })
+        });
 
         return response.json({ id });
     }
